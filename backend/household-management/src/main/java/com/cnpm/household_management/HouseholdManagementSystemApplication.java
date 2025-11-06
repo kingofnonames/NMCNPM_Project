5930d443c0401062bc638dc0fee28812bcd04044
NoNames <quocanhak57@gmail.com>
2025-11-06 13:23:58 +0700

    #5 backend setup

---
+++ backend/household-management/src/main/java/com/cnpm/household_management/HouseholdManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cnpm.household_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseholdManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/household-management/src/test/java/com/cnpm/household_management/HouseholdManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnpm.household_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
