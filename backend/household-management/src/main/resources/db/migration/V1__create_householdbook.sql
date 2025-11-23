6e0ea9eaed228504e9dc42d169e40290ca69a460
Đào Duy Anh <160158131+DuySakura@users.noreply.github.com>
2025-11-23 11:01:08 +0700

    Revert "Feature/#11 login register"

--- backend/household-management/src/main/resources/db/migration/V1__create_householdbook.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE HouseholdBook (
-    householdBookID  VARCHAR(20) NOT NULL PRIMARY KEY,   
-    address          VARCHAR(200) NOT NULL,
-    issueDate        DATE NOT NULL,
-    status           VARCHAR(200) NOT NULL
-);
