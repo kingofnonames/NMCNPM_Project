5124448872901c926e74ec4207ec0622e56fe4d3
Đào Duy Anh <160158131+DuySakura@users.noreply.github.com>
2025-11-27 21:41:52 +0700

    Revert "#9 databse-migration"

--- backend/household-management/src/main/resources/db/migration/V1__drop_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS UserData;
-DROP TABLE IF EXISTS TemporaryResidenceDeclaration;
-DROP TABLE IF EXISTS TemporaryAbsenceDeclaration;
-DROP TABLE IF EXISTS ResidentChange;
-DROP TABLE IF EXISTS HouseholdBookChange;
-DROP TABLE IF EXISTS HouseholdMember;
-DROP TABLE IF EXISTS Resident;
-DROP TABLE IF EXISTS HouseholdBook;
-
