4d1d4ff97ee79d9cda9bf2eec54690b6d01f995e
Namle <namlecyb@gmail.com>
2025-11-27 12:02:07 +0700

    #9 databse-migration

---
+++ backend/household-management/src/main/resources/db/migration/V1__drop_table.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS UserData;
+DROP TABLE IF EXISTS TemporaryResidenceDeclaration;
+DROP TABLE IF EXISTS TemporaryAbsenceDeclaration;
+DROP TABLE IF EXISTS ResidentChange;
+DROP TABLE IF EXISTS HouseholdBookChange;
+DROP TABLE IF EXISTS HouseholdMember;
+DROP TABLE IF EXISTS Resident;
+DROP TABLE IF EXISTS HouseholdBook;
+
