cb22c089442563368437305380deaa43ee3871b5
NoNames <quocanhak57@gmail.com>
2025-11-29 22:09:07 +0700

    #7 application reconfig

--- backend/household-management/src/main/resources/db/migration/V3__create_household_member.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE HouseholdMember (
-    householdBookID         VARCHAR(20) NOT NULL,
-    residentID              VARCHAR(20) NOT NULL,
-    role                    VARCHAR(200) NOT NULL, -- có thể là chủ hộ, vợ/ chồng, con cái, ...
-    PRIMARY KEY (householdBookID, residentID),
-    FOREIGN KEY (householdBookID) REFERENCES HouseholdBook(householdBookID),
-    FOREIGN KEY (residentID) REFERENCES Resident(residentID)
-);
